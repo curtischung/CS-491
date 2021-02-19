04e29e59f6a42a99ae114b153bce5b0779503adb
Curtis Chung <CChung@tmccadmn.tmcc.edu>
2021-02-19 15:19:14 -0800

    initial commit

---
+++ 2-Blackjack/blackjack.py
@@ -0,0 +1,14 @@
+
+class Blackjack:
+    def __init__ (self):
+        pass
+
+    def deal (self):
+        pass
+
+    def hit (self):
+
+
+
+if __name__ == "__main__":
+    print("Blackjack bitch, I win")
